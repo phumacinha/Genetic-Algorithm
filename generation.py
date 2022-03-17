7e0c2aaf71be0755be0b07e71becb95a615a041b
mateuscarvalhog <mateus.goncalves@estudante.ufla.br>
2022-03-17 18:16:24 -0300

    Generation representation changed

--- generation.py
+++ generation.py
@@ -3,20 +3,20 @@ import numpy as np
 from chromosome import Chromosome
 
 class Generation(object):
-    def __init__(self, individuals, parents, generation_number: int):
-        self.individuals = individuals
+    def __init__(self, chromosomes, parents, generation_number: int):
+        self.chromosomes = chromosomes
         self.parents = parents
         self.generation_number = generation_number
 
     def __repr__(self):
         photo = "-"*20 + "\nGeneration: " + str(self.generation_number) + "\n\n"
-        photo += "Individuals:\n"
-        for individual in self.individuals:
-            photo += str(individual) + "\n"
-        photo += "\nParents:\n"
+        photo += "Chromosomes:\n"
+        for chromosome in self.chromosomes:
+            photo += str(chromosome) + " "
+        photo += "\n\nParents:\n"
         for parent in self.parents:
-            photo += str(parent) + "\n"
-        photo += "-"*20
+            photo += str(parent) + " "
+        photo += "\n" + ("-"*20)
         return photo
 
 chromo1 = Chromosome.random(num_of_genes = 4)
